9810f4809f1b83861cecc62cd235be93adbb1093
Itatiano Filho <itaticontato@gmail.com>
2022-09-14 20:42:36 -0300

    falta 4,7,14

---
+++ paradigmas de programação python/exercicios 2/14(FALTA).py
@@ -0,0 +1,8 @@
+#14. A concessionária de veículos “CARANGO” está vendendo os seus veículos com desconto.
+#  Faça um algoritmo que calcule e exiba o valor do desconto e o valor a ser pago pelo cliente.
+#  O desconto deverá ser calculado sobre o valor do veículo de acordo com o combustível
+#  (álcool – 25%, gasolina – 21% ou diesel –14%). Com valor do veículo zero encerra entrada de dados.
+#  Informe total de desconto e total pago pelos clientes.
+
+print("\t\t\tCARANGO\t\t\t")
+
