765effdaa49f3a49aaacb667adbd2f6554500ac8
Itatiano Filho <itaticontato@gmail.com>
2022-09-15 19:54:59 -0300

    final commit exercicios

---
+++ paradigmas de programação python/exercicios 2/7.py
@@ -0,0 +1,8 @@
+#7. O custo ao consumidor de um carro novo é a soma do custo de fábrica com a percentagem do distribuidor
+#  e dos impostos (aplicados, primeiro os impostos sobre o custo de fábrica, e depois a percentagem do
+#  distribuidor sobre o resultado). Supondo que a percentagem do distribuidor seja de 28% e os impostos 45%.
+#  Escrever um algoritmo que leia o custo de fábrica de um carro e informe o custo ao consumidor do mesmo.
+
+fab = float (input("Custo do carro (vindo da fábrica):"))
+con = (fab * 1.45) * 1.28
+print ("Valor da venda (consumidor) é de:", con)
